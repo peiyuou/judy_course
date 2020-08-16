4a4a6b72aae9bb659f8eb86e2814cd946f1584bf
peiyuou <peiyuouyang0417@gmail.com>
2020-08-16 20:22:40 +0800

    購物車完成

--- js/header.js
+++ js/header.js
@@ -1,3 +1,10 @@
-$('.cart').click(function () {
-    $('.side_cart').toggleClass('cart_off');
+$(document).ready(function () {
+
+    $('.cart').click(function () {
+        $('.side_cart').toggleClass('cart_off');
+    });
+
+    $('#cart_close').click(function () {
+        $('.side_cart').addClass('cart_off');
+    });
 });
