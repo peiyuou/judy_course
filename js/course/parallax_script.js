6fd109de1fbc6f6eb7ef82160240bbe6b7f46ce1
peiyuou <peiyuouyang0417@gmail.com>
2020-08-21 00:47:00 +0800

    course_main加入社邊攔、para r完成

--- js/course/parallax_script.js
+++ js/course/parallax_script.js
@@ -18,16 +18,26 @@ function ScrollBG() {
         //开始为每个要动的元素设置左边距和上边距，以每个元素的不同zIndex值来区别移动量
         // for (var i = 0; i < oLi.length; i++) {
         //左边距和上边距的值可以随意调整
-        oLi[0].style.marginLeft = -(x - mx) / 70 * oLi[0].style.zIndex + 'px';
+        oLi[1].style.marginLeft = -(x - mx) / 70 * oLi[1].style.zIndex + 'px';
         // oLi[0].style.marginTop = (y - my) / 90 * oLi[0].style.zIndex + 'px';
 
-        oLi[1].style.marginLeft = (x - mx) / 40 * oLi[1].style.zIndex + 'px';
+        oLi[2].style.marginLeft = (x - mx) / 40 * oLi[2].style.zIndex + 'px';
         // oLi[1].style.marginTop = (y - my) / 90 * oLi[1].style.zIndex + 'px';
 
-        oLi[2].style.marginLeft = (x - mx) / 80 * oLi[2].style.zIndex + 'px';
+        oLi[3].style.marginLeft = (x - mx) / 80 * oLi[3].style.zIndex + 'px';
         // oLi[2].style.marginTop = (y - my) / 90 * oLi[2].style.zIndex + 'px';
         // }
     };
 }
+
+
+
+// function resize_background() {
+//     $(window).resize(function {
+//         $('#para_bg').style({
+//             wide:
+//         });
+//     })
+// }
 
 window.addEventListener('load', ScrollBG);
