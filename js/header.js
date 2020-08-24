fe06c7ac0c7cbb9f60eea013d2626a276867c037
peiyuou <peiyuouyang0417@gmail.com>
2020-08-24 18:21:05 +0800

    隱藏scroll bar成功~~

--- js/header.js
+++ js/header.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // header
 
-    $('.cart').click(function () {
+    $('.cart').click(function() {
         $('.side_cart').toggleClass('cart_off');
     });
 
-    $('#cart_close').click(function () {
+    $('#cart_close').click(function() {
         $('.side_cart').addClass('cart_off');
     });
     // ---------------------------
     // 漢堡
 
-    $(function () {
+    $(function() {
         // 漢堡
 
         // hamburger icon 的切換
-        $("button.hamburger").on("click", function () {
+        $("button.hamburger").on("click", function() {
             $(this).toggleClass("is-active");
         });
 
-        $("button.hamburger").on("click", function () {
+        $("button.hamburger").on("click", function() {
             $('body').toggleClass("scroll_disable");
         });
 
@@ -27,18 +27,27 @@ $(document).ready(function () {
 
 
     // ----------------
-    $(function () {
+    $(function() {
 
         // 點擊按鈕，選單縮放
-        $("button.btn_switch").on("click", function () {
+        $("button.btn_switch").on("click", function() {
             $(".header_nav").slideToggle();
         });
 
     });
 
-
-
-
-
-
 });
+
+
+// cart
+
+// window.addEventListener('load', function() {
+//     let car_btn = document.getElementsByClassName('add_cart_btn');
+
+//     for (i = 0; i < cart_btn.length; i++){
+//         car_btn[i].addEventListener('click', function (e) {
+//             course_name = e.target.parentNode.parentNode.parentNode.childNodes[]
+
+//         });
+//     };
+// });
